04b0cb9e31354c7990f5cd1fe9aaa69401b91208
elliot.yibaebi <e.yibaebi@genesystechhub.com>
2021-07-08 09:29:34 +0100

    build hamburger menu on small screen sizes

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,20 +1,38 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
 import PrimaryLogo from "../../public/images/primary-logo.svg";
 import WhiteLogoVariant from "../../public/images/white-logo-variant.svg";
+import CloseButton from "../../public/images/LandingPage/close_button.svg";
+import HamMenu from "../../public/images/LandingPage/hamburger_menu.svg";
 import navStyles from "../../styles/Navbar.module.css";
 
 const Navbar = () => {
   const [activeHash, setActiveHash] = useState("");
   const [changeNavbar, setChangeNavbar] = useState(false);
+  const [hamburgerMenu, setHamburgerMenu] = useState(false);
+  const hamMenuRef = useRef();
   const router = useRouter();
 
   useEffect(() => {
     setActiveHash(router.asPath);
     window.addEventListener("scroll", handleNavbarBgChange);
   });
+
+  useEffect(() => {
+    const listener = (event) => {
+      if (!hamMenuRef.current || hamMenuRef.current.contains(event.target)) {
+        return;
+      }
+      setHamburgerMenu(false);
+    };
+
+    document.addEventListener("mousedown", listener);
+    return () => {
+      document.removeEventListener("mousedown", listener);
+    };
+  }, [hamMenuRef]);
 
   const handleNavbarBgChange = () => {
     if (window.scrollY > 70) {
@@ -32,57 +50,76 @@ const Navbar = () => {
     <nav
       className={`${navStyles.container} ${
         changeNavbar && navStyles.container__white
-      } d-flex justify-content-between container-fluid p-fixed`}
+      }  d-flex justify-content-between container-fluid p-fixed`}
     >
       <div className="drm-logo-container">
         {changeNavbar ? <PrimaryLogo /> : <WhiteLogoVariant />}
       </div>
-      <ul className={`${navStyles.links__container}`}>
-        <li
-          className={`${navStyles.link__item} ${
-            (activeTab("#home") || activeTab("")) && navStyles.active__link
-          }`}
-        >
-          <Link href="#home">
-            <a>Home</a>
-          </Link>
-        </li>
-        <li
-          className={`${navStyles.link__item} ${
-            activeTab("#about") && navStyles.active__link
-          }`}
-        >
-          <Link href="#about">
-            <a>About us</a>
-          </Link>
-        </li>
-        <li
-          className={`${navStyles.link__item} ${
-            activeTab("#services") && navStyles.active__link
-          }`}
-        >
-          <Link href="#services">
-            <a>Services</a>
-          </Link>
-        </li>
-        <li
-          className={`${navStyles.link__item} ${
-            activeTab("#businesses") && navStyles.active__link
-          }`}
-        >
-          <Link href="#businesses">
-            <a>Our businesses</a>
-          </Link>
-        </li>
-        <li
-          className={`${navStyles.link__item} ${
-            activeTab("#contact") && navStyles.active__link
-          }`}
-        >
-          <Link href="#contact">
-            <a>Contact us</a>
-          </Link>
-        </li>
+      <div
+        onClick={() => setHamburgerMenu(true)}
+        className={`${navStyles.ham__menu}`}
+      >
+        <HamMenu />
+      </div>
+      <ul
+        className={`${navStyles.links__container} ${
+          hamburgerMenu ? navStyles.hamburger : ""
+        }`}
+      >
+        <div ref={hamMenuRef} className={`${navStyles.links__wrapper}`}>
+          <span
+            onClick={() => setHamburgerMenu(false)}
+            className={`${navStyles.close__button}`}
+          >
+            <CloseButton />
+          </span>
+
+          <li
+            className={`${navStyles.link__item} ${
+              (activeTab("#home") || activeTab("")) && navStyles.active__link
+            }`}
+          >
+            <Link href="#home">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li
+            className={`${navStyles.link__item} ${
+              activeTab("#about") && navStyles.active__link
+            }`}
+          >
+            <Link href="#about">
+              <a>About us</a>
+            </Link>
+          </li>
+          <li
+            className={`${navStyles.link__item} ${
+              activeTab("#services") && navStyles.active__link
+            }`}
+          >
+            <Link href="#services">
+              <a>Services</a>
+            </Link>
+          </li>
+          <li
+            className={`${navStyles.link__item} ${
+              activeTab("#businesses") && navStyles.active__link
+            }`}
+          >
+            <Link href="#businesses">
+              <a>Our businesses</a>
+            </Link>
+          </li>
+          <li
+            className={`${navStyles.link__item} ${
+              activeTab("#contact") && navStyles.active__link
+            }`}
+          >
+            <Link href="#contact">
+              <a>Contact us</a>
+            </Link>
+          </li>
+        </div>
       </ul>
     </nav>
   );
